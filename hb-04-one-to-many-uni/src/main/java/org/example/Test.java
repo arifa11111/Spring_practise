c80795c64e08ed5fe48364d5cb1969faeecd0b15
arifa11111 <101788554+arifa11111@users.noreply.github.com>
2022-05-26 11:04:30 +0530

    Add files via upload

---
+++ hb-04-one-to-many-uni/src/main/java/org/example/Test.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Test {
+    public static void main( String[] args ) {
+        String jdbcUrl="jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+        String user="hbstudent";
+        String pass ="Arifa@786";
+        try{
+            System.out.println("Connecting to DB : "+jdbcUrl);
+            Connection myConn= DriverManager.getConnection(jdbcUrl,user,pass);
+            System.out.println("Connection Successfull !!");
+
+        }catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+
+}
